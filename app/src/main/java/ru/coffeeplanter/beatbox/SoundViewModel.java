05d70d611b3994f927ab6c83baae35d869ae9cd8
IS <russland@yandex.ru>
2017-04-06 16:27:46 +0300

    Chapter 20 completed.

---
+++ app/src/main/java/ru/coffeeplanter/beatbox/SoundViewModel.java
@@ -0,0 +1,30 @@
+package ru.coffeeplanter.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+//        notifyChange();
+        notifyPropertyChanged(BR.title);
+    }
+
+}
